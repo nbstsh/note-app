c5878ad2190837b596f3151a971fec39924d98b1
Satoshi Inaba <satoshi@SatoshinoMacBook-puro.local>
2018-12-27 14:28:04 +0700

    set up the note-app project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+path = require('path')
+
+
+module.exports = {
+    entry: {
+        index: ['babel-polyfill', './src/index.js'],
+        edit: ['babel-polyfill', './src/edit.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: '[name]-bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        publicPath: '/scripts/'
+    }
+}
