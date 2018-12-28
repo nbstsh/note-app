3bd3500ec0daaacbef7e766d3be53dc8be205a43
Satoshi Inaba <satoshi@SatoshinoMacBook-puro.local>
2018-12-28 17:27:50 +0700

    initialize edit page

--- src/edit.js
+++ src/edit.js
@@ -1,1 +1,7 @@
-console.log('edit.js')
+import { getFilters, setFilters } from './filters'
+import { saveNotes, loadNotes, getNotes, createNote, upateNote, updateNote, resetNotes, removeNote } from './notes'
+import { generateNoteDOM, renderNotes, getFilteredNotes, generateLastEditedText, initializeEditPage } from './view'
+
+// Initialize edit Page
+const id = decodeURI(location.hash.substring(1))
+initializeEditPage(id)
