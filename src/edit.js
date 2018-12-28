523e9aa8097230c7dab97170e3ac2438da27f782
Satoshi Inaba <satoshi@SatoshinoMacBook-puro.local>
2018-12-28 17:46:29 +0700

    add event to update note

--- src/edit.js
+++ src/edit.js
@@ -5,3 +5,17 @@ import { generateNoteDOM, renderNotes, getFilteredNotes, generateLastEditedText,
 // Initialize edit Page
 const id = decodeURI(location.hash.substring(1))
 initializeEditPage(id)
+
+
+document.querySelector('#note-title').addEventListener('input', (e) => {
+    if (typeof e.target.value === 'string') {
+        updateNote({ id: id, title : e.target.value })
+    }
+})
+
+document.querySelector('#note-body').addEventListener('input', (e) => {
+    if (typeof e.target.value === 'string') {
+        console.log(e.target.value)
+        updateNote({ id: id, body: e.target.value})
+    }
+})
